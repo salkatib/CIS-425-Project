a391c1b2b33ea38318ab541fde01a0aa0e9368c4
salkatib <sga979@gmail.com>
2020-12-11 21:26:40 -0500

    Latest

---
+++ CIS 425 Database Website/scripts/scripts.js
@@ -0,0 +1,6 @@
+
+
+window.addEventListener("load", function() {
+  var year = new Date().getFullYear();
+  document.getElementById("yearName").innerHTML = "&copy; "   + year + " B.S.T. Smart Scholarship Committee";
+})
