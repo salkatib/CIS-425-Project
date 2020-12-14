bce95ae3efd65daffb713f6b420613260e47f3f4
salkatib <47203494+salkatib@users.noreply.github.com>
2020-12-13 21:13:48 -0500

    Delete scripts.js

--- CIS 425 Database Website/scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-
-
-window.addEventListener("load", function() {
-  var year = new Date().getFullYear();
-  document.getElementById("yearName").innerHTML = "&copy; "   + year + " B.S.T. Smart Scholarship Committee";
-})
